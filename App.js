50d7732d61c1bb1e578c13701fdd134b75bd255b
M.L.M Allafran <153066426+afran99@users.noreply.github.com>
2024-12-20 04:59:59 +0530

    create product view

--- App.js
+++ App.js
@@ -6,21 +6,80 @@ export default function App() {
 
     <View style={styles.view2}>
 
+      {/* Start Header part */}
+
       <LinearGradient colors={['#58b4ed', '#ffffff']}
         style={styles.Linear}
         start={{ x: 1, y: 0 }}
-        end={{ x: 0, y:  1}}  >
+        end={{ x: 0, y: 1 }}  >
         <View style={styles.view1}>
           <Image source={require('./assets/image/logo.png')} contentFit={"contain"} style={styles.logo} />
           <Text style={styles.logoText}>Smart Cooker</Text>
         </View >
       </LinearGradient>
+      {/* End Header part */}
 
+      {/* start recipies text view */}
       <View style={styles.view3}>
         <Text style={styles.rText}>Recipies</Text>
       </View>
+      {/* end recipies text view */}
 
-      <View style={styles.}></View>
+
+      {/* Start recipi view */}
+      <View style={styles.reView}>
+
+        {/* start producy*/}
+        <LinearGradient colors={['#01b4ed', '#88ff89']}
+          style={styles.proView}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 1 }}  >
+            <View style={styles.prDetailsView}>
+              <View><Text>Hello</Text></View>
+              <View><Text>Hello2</Text></View>
+            </View>
+
+
+
+        </LinearGradient>
+
+        <LinearGradient colors={['#01b4ed', '#88ff89']}
+          style={styles.proView}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 1 }}  >
+
+
+
+        </LinearGradient>
+
+        <LinearGradient colors={['#01b4ed', '#88ff89']}
+          style={styles.proView}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 1 }}  >
+
+
+
+
+        </LinearGradient>
+
+        <LinearGradient colors={['#01b4ed', '#88ff89']}
+          style={styles.proView}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 1 }}  >
+
+
+
+        </LinearGradient>
+        
+        <View >
+
+        </View>
+        {/* end produc */}
+
+      </View>
+
+      {/* Start recipi view */}
+
 
     </View>
 
@@ -50,7 +109,6 @@ const styles = StyleSheet.create({
   },
 
   view3: {
-    flex: 1,
     backgroundColor: "white",
     justifyContent: "flex-start",
   },
@@ -63,7 +121,7 @@ const styles = StyleSheet.create({
   logoText: {
     fontSize: 30,
     fontWeight: "bold"
-    fon
+
   },
 
   rText: {
@@ -71,7 +129,33 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginStart: 30,
     marginTop: 20,
+  },
+
+  reView: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+
+  },
+
+  proView: {
+    marginTop: 20,
+    height: "20%",
+    width: "90%",
+    borderRadius: 20,
+    shadowColor: "'black'",
+    shadowOffset: { width: 5, height: 5 },
+    shadowOpacity: 1,
+    shadowRadius: 3.84,
+  },
+
+  prDetailsView: {
+    flex:1,
+    flexDirection: "row",
+    justifyContent:"flex-start",
+    alignItems:"center",
   }
+
 
 
 });
